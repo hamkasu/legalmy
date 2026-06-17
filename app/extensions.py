53b07e94b018854c5486f390598466a29ad4463b
LegalMY Builder <lumkif@gmail.com>
2026-06-17 01:12:28 +0000

    Add Flask-Login user_loader callback
    - Register user_loader to load User from database by ID
    - Fixes 'Missing user_loader' error when rendering templates
    - Allows Flask-Login to manage current_user context

--- app/extensions.py
+++ app/extensions.py
@@ -14,3 +14,11 @@ limiter = None
 login_manager.login_view = 'auth.login'
 login_manager.login_message = 'Please log in to access this page.'
 login_manager.login_message_category = 'info'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    """Load user by ID from database."""
+    from app.models.user import User
+    return User.query.get(int(user_id))
+
